378b33786d584333871e1473101714e7f32818e4
zubrilla <pezdickurus@gmail.com>
2024-05-23 19:37:44 +0400

    Update QuizController.java

--- QuizController.java
+++ QuizController.java
@@ -7,8 +7,37 @@ import java.util.List;
 public class QuizController {
 
     private final List<Term> terms = Arrays.asList(
-            new Term("term1", "перевод1"),
-            new Term("term2", "перевод2"),
+            new Term("ala ae f", "крыло"),
+new Term("apertura ae f", "отверстие"),
+new Term("arteria ae f", "артерия"),
+new Term("aqua ae f", "вода"),
+new Term("bucca ae f", "щека"),
+new Term("bursa ae f", "сумка"),
+new Term("clavicula ae f", "ключица"),
+new Term("concha ae f", "раковина"),
+new Term("crista ae f", "гребень"),
+new Term("dentura ae f", "ряд зубов"),
+new Term("eminentia ae f", "возвышение"),
+new Term("fibula ae f", "малая берцовая кость"),
+new Term("fossa ae f", "ямка"),
+new Term("fractura ae f", "перелом"),
+new Term("gingiva ae f", "десна"),
+new Term("glandula ae f", "железа"),
+new Term("gutta ae f", "капля"),
+new Term("incisura ae f", "вырезка"),
+new Term("lamina ae f", "пластинка"),
+new Term("linea ae f", "линия"),
+new Term("lingua ae f", "язык"),
+new Term("mandibula ae f", "нижняя челюсть"),
+new Term("maxilla ae f", "верхняя челюсть"),
+new Term("medulla ae f", "спинной мозг"),
+new Term("papilla ae f", "сосочек"),
+new Term("plica ae f", "складка"),
+new Term("pulpa ae f", "пульпа"),
+new Term("pustula ae f", "гнойничок"),
+new Term("rima ae f", "щель"),
+new Term("saliva ae f", "слюна"),
+
             // Добавьте остальные термины и их переводы
     );
 
