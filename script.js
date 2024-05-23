1312d12ba9856cf5dbf58d485f1d46e5c2becdfe
zubrilla <pezdickurus@gmail.com>
2024-05-23 19:55:19 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("startButton").addEventListener("click", function() {
-    // Отправить запрос на бэкэнд для начала теста
-    fetch("/startQuiz")
-        .then(response => response.json())
-        .then(data => {
-            // Отобразить вопрос теста пользователю
-            document.getElementById("output").innerText = data.question;
-        });
-});
